4eec9b52793ff38f4e58eb13ecaf5d6e60bdcdf3
czy <czy0218@163.com>
2019-02-28 17:27:13 +0800

    add 分配算法

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.algorithm.Distribution;
 import com.example.demo.factory.abstr.MilkFactory;
 import com.example.demo.factory.func.Factoty;
 import com.example.demo.factory.func.TelunsuFactory;
@@ -11,6 +12,11 @@ import com.example.demo.proxy.Son;
 import com.example.demo.proxy.Zhongji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -48,6 +54,91 @@ public class DemoApplication {
 
         person1.job();
         person1.findLove();
+        System.out.println("========================--end");
+
+        System.out.println("========================权重分配");
+        Map<String, String> users1 = new HashMap<>();
+        users1.put("a", "10");
+        users1.put("b", "10");
+        users1.put("c", "20");
+        users1.put("d", "10");
+        users1.put("e", "50");
+        List<String> tasks1 = new ArrayList<String>();
+        tasks1.add("1");
+        tasks1.add("2");
+        tasks1.add("3");
+        tasks1.add("4");
+        tasks1.add("5");
+        tasks1.add("6");
+        tasks1.add("7");
+        tasks1.add("8");
+        tasks1.add("9");
+        tasks1.add("10");
+        tasks1.add("11");
+        tasks1.add("12");
+        tasks1.add("13");
+        tasks1.add("14");
+        tasks1.add("15");
+        tasks1.add("16");
+        tasks1.add("17");
+        tasks1.add("18");
+        tasks1.add("19");
+        Map<String, List<String>> stringListMap1 = Distribution.allotOfProportion(users1, tasks1);
+
+        stringListMap1.forEach((v, k) -> {
+            System.out.println(v + "====" + k);
+        });
+
+
+        System.out.println("========================平均分配");
+        List<String> users = new ArrayList<String>();
+        users.add("a");
+        users.add("b");
+        users.add("c");
+        users.add("d");
+        users.add("e");
+        List<String> tasks = new ArrayList<String>();
+        tasks.add("1");
+        tasks.add("2");
+        tasks.add("3");
+        tasks.add("4");
+        tasks.add("5");
+        tasks.add("6");
+        tasks.add("7");
+        tasks.add("8");
+        tasks.add("9");
+        tasks.add("10");
+        tasks.add("11");
+        tasks.add("12");
+        tasks.add("13");
+        tasks.add("14");
+        tasks.add("15");
+        tasks.add("16");
+        tasks.add("17");
+        tasks.add("18");
+        tasks.add("19");
+
+        Map<String, List<String>> stringListMap = Distribution.allotOfAverage(users, tasks);
+        stringListMap.get(1);
+        stringListMap.forEach((k, v) -> {
+            System.out.println(k + "==========" + v);
+        });
+
+
+        System.out.println("========================全都分配");
+        List<String> users2 = new ArrayList<>();
+        users2.add("1");
+        users2.add("2");
+        users2.add("3");
+        List<String> tasks2 = new ArrayList<>();
+        tasks2.add("a");
+        tasks2.add("b");
+        tasks2.add("c");
+        Map<String, List<String>> map = Distribution.allotOfAll(users2, tasks2);
+
+        map.forEach((v, k) -> {
+            System.out.println(v + "====" + k);
+        });
 
     }
 
---
+++ src/main/java/com/example/demo/algorithm/Distribution.java
@@ -0,0 +1,96 @@
+package com.example.demo.algorithm;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Author: czy
+ * @Date: 2019/2/28 15:52
+ * @Version 1.0
+ */
+public class Distribution {
+
+    /*
+     * 随机分配
+     */
+    public static Map<String, List<String>> allotOfRandom(List<String> users, List<String> tasks) {
+        Map<String, List<String>> allot = new ConcurrentHashMap<String, List<String>>(); //保存分配的信息
+        if (users != null && users.size() > 0 && tasks != null && tasks.size() > 0) {
+            for (int i = 0; i < tasks.size(); i++) {
+                int r_user = new Random().nextInt(users.size());
+                if (allot.containsKey(users.get(r_user))) {
+                    List<String> list = allot.get(users.get(r_user));
+                    list.add(tasks.get(i));
+                    allot.put(users.get(r_user), list);
+                } else {
+                    List<String> list = new ArrayList<String>();
+                    list.add(tasks.get(i));
+                    allot.put(users.get(r_user), list);
+                }
+            }
+        }
+        return allot;
+    }
+
+    /*
+     * 平均分配
+     */
+    public static Map<String, List<String>> allotOfAverage(List<String> users, List<String> tasks) {
+        Map<String, List<String>> allot = new ConcurrentHashMap<String, List<String>>(); //保存分配的信息
+        if (users != null && users.size() > 0 && tasks != null && tasks.size() > 0) {
+            for (int i = 0; i < tasks.size(); i++) {
+                int j = i % users.size();
+                if (allot.containsKey(users.get(j))) {
+                    List<String> list = allot.get(users.get(j));
+                    list.add(tasks.get(i));
+                    allot.put(users.get(j), list);
+                } else {
+                    List<String> list = new ArrayList<String>();
+                    list.add(tasks.get(i));
+                    allot.put(users.get(j), list);
+                }
+            }
+        }
+        return allot;
+    }
+
+    /*
+     * 权重分配
+     */
+    public static Map<String, List<String>> allotOfProportion(Map<String, String> users, List<String> tasks) {
+        Map<String, List<String>> allot = new ConcurrentHashMap<String, List<String>>(); //保存分配的信息
+        if (users != null && users.size() > 0 && tasks != null && tasks.size() > 0) {
+            int a = 0;//总权重
+            for (Map.Entry<String, String> entry : users.entrySet()) {
+                a += Integer.parseInt(entry.getValue());
+            }
+            int start = 0, end = 0;//起始下标 ,结束下标
+            if (a > 0) {
+                for (Map.Entry<String, String> entry : users.entrySet()) {
+                    List<String> allotTask = new ArrayList<String>();
+                    end += Integer.parseInt(entry.getValue());//权重累计
+                    for (; start < tasks.size() * end / a; start++) {
+                        allotTask.add(tasks.get(start));
+                    }
+                    allot.put(entry.getKey(), allotTask);
+                }
+            }
+        }
+        return allot;
+    }
+
+    /*
+    * 全部分配
+    * */
+    public static Map<String, List<String>> allotOfAll(List<String> users, List<String> tasks) {
+        Map<String, List<String>> map = new ConcurrentHashMap<String, List<String>>();
+        users.forEach(v -> {
+            List<String> list = new ArrayList();
+            tasks.forEach(k -> {
+                list.add(k);
+            });
+            map.put(v, list);
+        });
+        return map;
+    }
+}
